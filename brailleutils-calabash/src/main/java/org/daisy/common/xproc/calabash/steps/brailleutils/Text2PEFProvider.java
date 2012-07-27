93615feee4cdb7200e5abea4fa8d21568206af0b
Bert Frees <bert.frees@sbs.ch>
2012-07-27 16:53:13 +0200

    Simplifying calabash extensions

--- brailleutils-calabash/src/main/java/org/daisy/common/xproc/calabash/steps/brailleutils/Text2PEFProvider.java
+++ brailleutils-calabash/src/main/java/org/daisy/common/xproc/calabash/steps/brailleutils/Text2PEFProvider.java
@@ -1,9 +1,23 @@
 package org.daisy.common.xproc.calabash.steps.brailleutils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+
+import net.sf.saxon.s9api.QName;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XdmNode;
+
+import org.daisy.braille.pef.TextHandler;
 import org.daisy.common.xproc.calabash.XProcStepProvider;
 
 import com.xmlcalabash.core.XProcRuntime;
 import com.xmlcalabash.core.XProcStep;
+import com.xmlcalabash.io.ReadablePipe;
+import com.xmlcalabash.io.WritablePipe;
+import com.xmlcalabash.library.DefaultStep;
 import com.xmlcalabash.runtime.XAtomicStep;
 
 public class Text2PEFProvider implements XProcStepProvider {
@@ -11,5 +25,67 @@ public class Text2PEFProvider implements XProcStepProvider {
 	@Override
 	public XProcStep newStep(XProcRuntime runtime, XAtomicStep step) {
 		return new Text2PEF(runtime, step);
+	}
+
+	public static class Text2PEF extends DefaultStep {
+
+		private static final QName _temp_dir = new QName("temp-dir");
+
+		private ReadablePipe source = null;
+		private WritablePipe result = null;
+
+		private Text2PEF(XProcRuntime runtime, XAtomicStep step) {
+			super(runtime, step);
+		}
+
+		@Override
+		public void setInput(String port, ReadablePipe pipe) {
+			source = pipe;
+		}
+
+		@Override
+		public void setOutput(String port, WritablePipe pipe) {
+			result = pipe;
+		}
+
+		@Override
+		public void reset() {
+			source.resetReader();
+			result.resetWriter();
+		}
+
+		@Override
+		public void run() throws SaxonApiException {
+			super.run();
+
+			try {
+
+				File tempDir = new File(new URI(getOption(_temp_dir).getString()));
+				XdmNode text = source.read();
+
+				// Write text document to file
+				File textFile = File.createTempFile("text2pef.", ".txt", tempDir);
+				OutputStream textStream = new FileOutputStream(textFile);
+				OutputStreamWriter writer = new OutputStreamWriter(textStream, "UTF-8");
+				writer.write(text.getStringValue());
+				writer.close();
+
+				// Parse text to PEF
+				File pefFile = File.createTempFile("text2pef.", ".pef", tempDir);
+				TextHandler.Builder b = new TextHandler.Builder(textFile, pefFile);
+				b.converterId("org_daisy.EmbosserTableProvider.TableType.NABCC");
+				TextHandler handler = b.build();
+				handler.parse();
+				textFile.delete();
+
+				// Read PEF document
+				XdmNode pef = runtime.getProcessor().newDocumentBuilder().build(pefFile);
+				pefFile.delete();
+				result.write(pef);
+
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 }
--- brailleutils-calabash/src/main/resources/xml/xproc/library.xpl
+++ brailleutils-calabash/src/main/resources/xml/xproc/library.xpl
@@ -4,28 +4,10 @@
     xmlns:pef="http://xmlcalabash.com/ns/extensions/brailleutils"
     version="1.0">
     
-    <p:declare-step type="pef:emboss">
-        <p:input port="source" sequence="true"/>
-        <p:output port="result" sequence="true"/>
-        <p:option name="message" required="true"/>
-    </p:declare-step>
-    
-    <p:declare-step type="pef:pef2text">
-        <p:input port="source" sequence="true"/>
-        <p:output port="result" sequence="true"/>
-        <p:option name="message" required="true"/>
-    </p:declare-step>
-    
     <p:declare-step type="pef:text2pef">
         <p:input port="source" sequence="false" primary="true"/>
         <p:output port="result" sequence="false" primary="true"/>
         <p:option name="temp-dir" required="true" px:output="temp" px:sequence="false" px:type="anyDirURI"/>
-    </p:declare-step>
-    
-    <p:declare-step type="pef:validate">
-        <p:input port="source" sequence="true"/>
-        <p:output port="result" sequence="true"/>
-        <p:option name="message" required="true"/>
     </p:declare-step>
 </p:library>
 
