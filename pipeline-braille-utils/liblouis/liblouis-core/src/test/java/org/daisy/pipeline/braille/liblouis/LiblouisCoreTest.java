8ed7e3eb00687c0bd2798351aae227be6b5ba749
Bert Frees <bert.frees@sbs.ch>
2014-01-10 12:32:50 +0100

    Add test for liblouis-core (Pax Exam)

---
+++ pipeline-braille-utils/liblouis/liblouis-core/src/test/java/org/daisy/pipeline/braille/liblouis/LiblouisCoreTest.java
@@ -0,0 +1,53 @@
+package org.daisy.pipeline.braille.liblouis;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.ops4j.pax.exam.util.PathUtils;
+
+import static org.ops4j.pax.exam.CoreOptions.bundle;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class LiblouisCoreTest {
+	
+	@Inject
+	Liblouis liblouis;
+	
+	@Configuration
+	public Option[] config() {
+		return options(
+			systemProperty("logback.configurationFile").value("file:" + PathUtils.getBaseDir() + "/src/test/resources/logback.xml"),
+			mavenBundle().groupId("org.slf4j").artifactId("slf4j-api").version("1.7.2"),
+			mavenBundle().groupId("ch.qos.logback").artifactId("logback-core").version("1.0.11"),
+			mavenBundle().groupId("ch.qos.logback").artifactId("logback-classic").version("1.0.11"),
+			mavenBundle().groupId("org.apache.felix").artifactId("org.apache.felix.scr").version("1.6.2"),
+			mavenBundle().groupId("com.google.guava").artifactId("guava").versionAsInProject(),
+			mavenBundle().groupId("net.java.dev.jna").artifactId("jna").versionAsInProject(),
+			mavenBundle().groupId("org.liblouis").artifactId("liblouis-java").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.pipeline.modules.braille").artifactId("common-java").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.pipeline.modules.braille").artifactId("liblouis-native").versionAsInProject().classifier("linux"),
+			bundle("reference:file:" + PathUtils.getBaseDir() + "/target/classes/"),
+			bundle("reference:file:" + PathUtils.getBaseDir() + "/target/test-classes/table_paths/"),
+			junitBundles()
+		);
+	}
+	
+	@Test
+	public void testTranslate() {
+		assertEquals("foobar", liblouis.translate("foobar.cti", "foobar", false, null));
+	}
+}
