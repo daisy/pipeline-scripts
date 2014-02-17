c372b470f0ed0a44dfb8f87e9a96d2c87b4cc326
Bert Frees <bert.frees@sbs.ch>
2014-02-17 13:26:14 +0100

    .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,9 @@
 ((nil
-  (compile-command . (format "cd %s && mvn clean install"
-                             (locate-dominating-file buffer-file-name "pom.xml")))))
+  . ((compile-command . (format "cd %s && mvn clean install"
+                                (locate-dominating-file buffer-file-name "pom.xml")))))
+ (java-mode
+  . ((indent-tabs-mode . t)
+     (tab-width . 4))))
 
 
 
