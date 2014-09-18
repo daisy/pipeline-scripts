1d4aa85088cb2c8609d2b689b71379d8e359b86c
Bert Frees <bertfrees@gmail.com>
2014-09-18 20:04:00 +0200

    dotify-calabash: translate generated numbers to braille

--- pipeline-braille-utils/dotify/dotify-calabash/src/main/java/org/daisy/pipeline/braille/dotify/calabash/BypassTranslatorFactoryService.java
+++ pipeline-braille-utils/dotify/dotify-calabash/src/main/java/org/daisy/pipeline/braille/dotify/calabash/BypassTranslatorFactoryService.java
@@ -56,13 +56,39 @@ public class BypassTranslatorFactoryService implements BrailleTranslatorFactoryS
 		
 		private final static Pattern softHyphens = Pattern.compile("[\u00ad\u200b]+");
 		
+		private final static String[] digitTable = new String[]{
+			"\u281a","\u2801","\u2803","\u2809","\u2819","\u2811","\u280b","\u281b","\u2813","\u280a"};
+		
+		private static String translateInteger(int integer) {
+			StringBuilder sb = new StringBuilder();
+			sb.append("\u283c");
+			if (integer == 0)
+				sb.append(digitTable[0]);
+			while (integer > 0) {
+				sb.insert(1, digitTable[integer % 10]);
+				integer = integer / 10; }
+			return sb.toString();
+		}
+		
 		private static String filterOutSoftHyphens(String text) {
 			return softHyphens.matcher(text).replaceAll("");
 		}
 		
+		private String doTranslate(String text) {
+			if (" ".equals(text))
+				return "\u2800"; // for calculating margin character
+				                 // (see org.daisy.dotify.formatter.impl.FormatterContext)
+			try {
+				return translateInteger(Integer.parseInt(text)); } // for translating page numbers
+			catch (Exception e) {}
+			if (isHyphenating())
+				return text;
+			return filterOutSoftHyphens(text);
+		}
+		
 		public BrailleTranslatorResult translate(String text) {
 			
-			final BreakPointHandler bph = new BreakPointHandler(isHyphenating() ? text : filterOutSoftHyphens(text));
+			final BreakPointHandler bph = new BreakPointHandler(doTranslate(text));
 			
 			return new BrailleTranslatorResult() {
 				public String nextTranslatedRow(int limit, boolean force) {
