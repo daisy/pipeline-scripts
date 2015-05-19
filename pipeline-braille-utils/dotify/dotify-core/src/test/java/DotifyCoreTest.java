e0f181ba1142f00ea371bc68ec9e673ba0956beb
Bert Frees <bertfrees@gmail.com>
2015-05-19 20:58:06 +0200

    Add DotifyHyphenator

---
+++ pipeline-braille-utils/dotify/dotify-core/src/test/java/DotifyCoreTest.java
@@ -0,0 +1,71 @@
+import javax.inject.Inject;
+
+import org.daisy.pipeline.braille.dotify.DotifyHyphenator;
+import org.daisy.pipeline.braille.dotify.DotifyTranslator;
+
+import static org.daisy.pipeline.pax.exam.Options.brailleModule;
+import static org.daisy.pipeline.pax.exam.Options.bundlesAndDependencies;
+import static org.daisy.pipeline.pax.exam.Options.domTraversalPackage;
+import static org.daisy.pipeline.pax.exam.Options.felixDeclarativeServices;
+import static org.daisy.pipeline.pax.exam.Options.logbackBundles;
+import static org.daisy.pipeline.pax.exam.Options.logbackConfigFile;
+import static org.daisy.pipeline.pax.exam.Options.thisBundle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class DotifyCoreTest {
+	
+	@Inject
+	DotifyTranslator.Provider provider;
+	
+	@Inject
+	DotifyHyphenator.Provider hyphenatorProvider;
+	
+	@Configuration
+	public Option[] config() {
+		return options(
+			logbackConfigFile(),
+			domTraversalPackage(),
+			logbackBundles(),
+			felixDeclarativeServices(),
+			mavenBundle().groupId("com.google.guava").artifactId("guava").versionAsInProject(),
+			mavenBundle().groupId("com.googlecode.texhyphj").artifactId("texhyphj").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.api").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.common").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.translator.impl").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.hyphenator.impl").versionAsInProject(),
+			mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.antlr-runtime").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.libs").artifactId("jstyleparser").versionAsInProject(),
+			bundlesAndDependencies("org.daisy.pipeline.calabash-adapter"),
+			brailleModule("common-utils"),
+			brailleModule("css-core"),
+			thisBundle("org.daisy.pipeline.modules.braille", "dotify-core"),
+			junitBundles()
+		);
+	}
+	
+	@Test
+	public void testTranslate() {
+		assertEquals("⠋⠕⠕⠃⠁⠗", provider.get("(locale:sv_SE)").iterator().next().transform("foobar"));
+	}
+	
+	@Test
+	public void testHyphenate() {
+		assertEquals("foo\u00ADbar", (hyphenatorProvider.get("(locale:sv_SE)").iterator().next()).transform("foobar"));
+	}
+}
