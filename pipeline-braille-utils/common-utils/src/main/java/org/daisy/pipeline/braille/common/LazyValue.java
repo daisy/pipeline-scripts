snaekobbi/pipeline-mod-braille

liblouis-core: table discovery via liblouis API instead of manifest file
- LiblouisTablePath: deprecated manifest property
- removed LiblouisTableProvider interface which was solely locale based
- LiblouisTableRegistry: added onPathChange (see also
  onLiblouisTablePathUpdate which was removed in commit 0d3694bcc)
- LiblouisJnaImpl: re-index liblouis tables whenever table path
  changes and pass table queries on to lou_findTable

pipeline-braille-utils/common-utils/src/main/java/org/daisy/pipeline/braille/common/LazyValue.java
@@ -1,0 +1,75 @@
+package org.daisy.pipeline.braille.common;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.daisy.pipeline.braille.common.util.Function0;
+
+public abstract class LazyValue<V> implements Function0<V>, Iterable<V> {
+	
+	public V get() {
+		return get();
+	}
+	
+	public Iterator<V> iterator() {
+		return new Iterator<V>() {
+			boolean hasNext = true;
+			public boolean hasNext() {
+				return hasNext;
+			}
+			public V next() {
+				if (!hasNext())
+					throw new NoSuchElementException();
+				hasNext = false;
+				return get();
+			}
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+	
+	public static <V> LazyValue<V> from(final Function0<V> get) {
+		return new LazyValue<V>() {
+			public V apply() {
+				return get.apply();
+			}
+		};
+	}
+	
+	public static abstract class ImmutableLazyValue<V> extends LazyValue<V> {
+		
+		private V value = null;
+		protected boolean computed = false;
+		
+		public V get() {
+			if (!computed) {
+				value = apply();
+				computed = true; }
+			return value;
+		}
+		
+		public static <V> LazyValue<V> from(final Function0<V> get) {
+			return new ImmutableLazyValue<V>() {
+				public V apply() {
+					return get.apply();
+				}
+			};
+		}
+	}
+	
+	public static abstract class CachedLazyValue<V> extends ImmutableLazyValue<V> {
+		
+		public void invalidateCache() {
+			computed = false;
+		}
+		
+		public static <V> CachedLazyValue<V> from(final Function0<V> get) {
+			return new CachedLazyValue<V>() {
+				public V apply() {
+					return get.apply();
+				}
+			};
+		}
+	}
+}

pipeline-braille-utils/liblouis/liblouis-core/src/test/java/org/daisy/pipeline/braille/liblouis/LiblouisCoreTest.java
@@ -37,6 +37,7 @@ import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
@@ -47,9 +48,6 @@ public class LiblouisCoreTest {
 	
 	@Inject
 	LiblouisTableResolver resolver;
-	
-	@Inject
-	LiblouisTableProvider tableProvider;
 	
 	@Configuration
 	public Option[] config() {
@@ -66,7 +64,9 @@ public class LiblouisCoreTest {
 			bundlesAndDependencies("org.daisy.pipeline.calabash-adapter"),
 			brailleModule("common-utils"),
 			brailleModule("css-core"),
-			forThisPlatform(brailleModule("liblouis-native")),
+			// depends on https://github.com/liblouis/liblouis/pull/41
+			systemProperty("org.daisy.pipeline.liblouis.external").value("true"),
+			// forThisPlatform(brailleModule("liblouis-native")),
 			thisBundle(true),
 			bundle("reference:file:" + PathUtils.getBaseDir() + "/target/test-classes/table_paths/"),
 			junitBundles()
@@ -84,12 +84,6 @@ public class LiblouisCoreTest {
 	}
 	
 	@Test
-	public void testGetTableFromLocale() {
-		assertEquals(new URI[]{asURI("http://test/table_path_1/foobar.cti")}, tableProvider.get(parseLocale("foo")).iterator().next());
-		assertNull(Iterables.<URI[]>getFirst(tableProvider.get(parseLocale("bar")), null));
-	}
-	
-	@Test
 	public void testGetTranslatorFromQuery1() {
 		liblouis.get("(locale:foo)").iterator().next();
 	}
@@ -97,6 +91,11 @@ public class LiblouisCoreTest {
 	@Test
 	public void testGetTranslatorFromQuery2() {
 		liblouis.get("(table:'foobar.cti')").iterator().next();
+	}
+	
+	@Test
+	public void testGetTranslatorFromQuery3() {
+		liblouis.get("(locale:foo_BAR)").iterator().next();
 	}
 	
 	@Test

daisy/pipeline-scripts

pipeline-braille-utils/common-utils/src/main/java/org/daisy/pipeline/braille/common/LazyValue.java
@@ -1,0 +1,79 @@
+package org.daisy.pipeline.braille.common;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.daisy.pipeline.braille.common.util.Function0;
+
+public abstract class LazyValue<V> implements Function0<V>, Iterable<V> {
+	
+	public abstract V get();
+	
+	public V apply() {
+		return get();
+	}
+	
+	public Iterator<V> iterator() {
+		return new Iterator<V>() {
+			boolean hasNext = true;
+			public boolean hasNext() {
+				return hasNext;
+			}
+			public V next() {
+				if (!hasNext())
+					throw new NoSuchElementException();
+				hasNext = false;
+				return get();
+			}
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+	
+	public static <V> LazyValue<V> from(final Function0<V> get) {
+		return new LazyValue<V>() {
+			public V get() {
+				return get.apply();
+			}
+		};
+	}
+	
+	public static abstract class ImmutableLazyValue<V> extends LazyValue<V> {
+		
+		private V value = null;
+		protected boolean computed = false;
+		
+		public V get() {
+			if (!computed) {
+				value = delegate();
+				computed = true; }
+			return value;
+		}
+		
+		protected abstract V delegate();
+		
+		public static <V> LazyValue<V> from(final Function0<V> get) {
+			return new ImmutableLazyValue<V>() {
+				public V delegate() {
+					return get.apply();
+				}
+			};
+		}
+	}
+	
+	public static abstract class CachedLazyValue<V> extends ImmutableLazyValue<V> {
+		
+		public void invalidateCache() {
+			computed = false;
+		}
+		
+		public static <V> CachedLazyValue<V> from(final Function0<V> get) {
+			return new CachedLazyValue<V>() {
+				public V delegate() {
+					return get.apply();
+				}
+			};
+		}
+	}
+}

pipeline-braille-utils/liblouis/liblouis-core/src/test/java/org/daisy/pipeline/braille/liblouis/LiblouisCoreTest.java
@@ -1,12 +1,8 @@
 package org.daisy.pipeline.braille.liblouis;
 
-import java.net.URI;
 import javax.inject.Inject;
 
-import com.google.common.collect.Iterables;
-
 import static org.daisy.pipeline.braille.common.util.Files.asFile;
-import static org.daisy.pipeline.braille.common.util.Locales.parseLocale;
 import static org.daisy.pipeline.braille.common.util.URIs.asURI;
 import org.daisy.pipeline.braille.liblouis.LiblouisTranslator.Typeform;
 
@@ -23,7 +19,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.junit.PaxExam;
@@ -36,6 +31,7 @@ import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
@@ -49,9 +45,6 @@ public class LiblouisCoreTest {
 	
 	@Inject
 	LiblouisTableResolver resolver;
-	
-	@Inject
-	LiblouisTableRegistry tableRegistry;
 	
 	@Configuration
 	public Option[] config() {
@@ -68,7 +61,9 @@ public class LiblouisCoreTest {
 			bundlesAndDependencies("org.daisy.pipeline.calabash-adapter"),
 			brailleModule("common-utils"),
 			brailleModule("css-core"),
-			forThisPlatform(brailleModule("liblouis-native")),
+			// depends on https://github.com/liblouis/liblouis/pull/41
+			systemProperty("org.daisy.pipeline.liblouis.external").value("true"),
+			// forThisPlatform(brailleModule("liblouis-native")),
 			thisBundle("org.daisy.pipeline.modules.braille", "liblouis-core"),
 			bundle("reference:file:" + PathUtils.getBaseDir() + "/target/test-classes/table_paths/"),
 			junitBundles()
@@ -86,12 +81,6 @@ public class LiblouisCoreTest {
 	}
 	
 	@Test
-	public void testGetTableFromLocale() {
-		assertEquals(new URI[]{asURI("http://test/table_path_1/foobar.cti")}, tableRegistry.get(parseLocale("foo")).iterator().next().asURIs());
-		assertNull(Iterables.<LiblouisTable>getFirst(tableRegistry.get(parseLocale("bar")), null));
-	}
-	
-	@Test
 	public void testGetTranslatorFromQuery1() {
 		provider.get("(locale:foo)").iterator().next();
 	}
@@ -99,6 +88,11 @@ public class LiblouisCoreTest {
 	@Test
 	public void testGetTranslatorFromQuery2() {
 		provider.get("(table:'foobar.cti')").iterator().next();
+	}
+	
+	@Test
+	public void testGetTranslatorFromQuery3() {
+		provider.get("(locale:foo_BAR)").iterator().next();
 	}
 	
 	@Test