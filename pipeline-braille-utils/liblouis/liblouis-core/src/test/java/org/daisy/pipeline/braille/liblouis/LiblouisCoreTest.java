snaekobbi/pipeline-mod-braille

Make LiblouisTranslator implement BrailleTranslator by always adding unicode.dis

pipeline-braille-utils/liblouis/liblouis-core/src/test/java/org/daisy/pipeline/braille/liblouis/LiblouisCoreTest.java
@@ -100,33 +100,32 @@ public class LiblouisCoreTest {
 	
 	@Test
 	public void testTranslate() {
-		assertEquals("foobar", liblouis.get("(table:'foobar.cti')").iterator().next().transform("foobar"));
+		assertEquals("⠋⠕⠕⠃⠁⠗", liblouis.get("(table:'foobar.cti')").iterator().next().transform("foobar"));
 	}
 	
 	@Test
 	public void testTranslateStyled() {
-		assertEquals("foobar", liblouis.get("(table:'foobar.cti')").iterator().next().transform("foobar", Typeform.ITALIC));
+		assertEquals("⠋⠕⠕⠃⠁⠗", liblouis.get("(table:'foobar.cti')").iterator().next().transform("foobar", Typeform.ITALIC));
 	}
 	
 	@Test
 	public void testTranslateSegments() {
 		LiblouisTranslator translator = liblouis.get("(table:'foobar.cti')").iterator().next();
-		assertEquals(new String[]{"foo","bar"}, translator.transform(new String[]{"foo","bar"}));
-		assertEquals(new String[]{"foo","","bar"}, translator.transform(new String[]{"foo","","bar"}));
+		assertEquals(new String[]{"⠋⠕⠕","⠃⠁⠗"}, translator.transform(new String[]{"foo","bar"}));
+		assertEquals(new String[]{"⠋⠕⠕","","⠃⠁⠗"}, translator.transform(new String[]{"foo","","bar"}));
 	}
 	
 	@Test
 	public void testTranslateSegmentsFuzzy() {
 		LiblouisTranslator translator = liblouis.get("(table:'foobar.ctb')").iterator().next();
-		assertEquals(new String[]{"fu","bar"}, translator.transform(new String[]{"foo","bar"}));
-		assertEquals(new String[]{"fu","bar"}, translator.transform(new String[]{"fo","obar"}));
-		assertEquals(new String[]{"fu","","bar"}, translator.transform(new String[]{"fo","","obar"}));
-		assertEquals(new String[]{"x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ",
-		                          "x ", "x ", "x ", "x ", "fu", "bar"},
+		assertEquals(new String[]{"⠋⠥","⠃⠁⠗"}, translator.transform(new String[]{"foo","bar"}));
+		assertEquals(new String[]{"⠋⠥","⠃⠁⠗"}, translator.transform(new String[]{"fo","obar"}));
+		assertEquals(new String[]{"⠋⠥","","⠃⠁⠗"}, translator.transform(new String[]{"fo","","obar"}));
+		assertEquals(new String[]{"⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ",
+		                          "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠋⠥", "⠃⠁⠗"},
 		             translator.transform(new String[]{
 		                          "x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ",
 		                          "x ", "x ", "x ", "x ", "fo", "obar"}));
-
 	}
 	
 	@Test
@@ -142,7 +141,7 @@ public class LiblouisCoreTest {
 	@Test
 	public void testTranslateAndHyphenateSomeSegments() {
 		LiblouisTranslator translator = liblouis.get("(table:'foobar.cti,foobar.dic')").iterator().next();
-		assertEquals(new String[]{"foo\u00ADbar ","foobar"},
+		assertEquals(new String[]{"⠋⠕⠕\u00AD⠃⠁⠗ ","⠋⠕⠕⠃⠁⠗"},
 		             translator.transform(new String[]{"foobar ","foobar"}, new String[]{"hyphens:auto","hyphens:none"}));
 	}
 }

pipeline-braille-utils/liblouis/liblouis-tables/src/test/java/LiblouisTablesTest.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 
 import static org.daisy.pipeline.braille.common.util.URLs.asURL;
 import org.daisy.pipeline.braille.liblouis.Liblouis;
+import static org.daisy.pipeline.braille.liblouis.LiblouisTablePath.serializeTable;
 
 import static org.daisy.pipeline.pax.exam.Options.brailleModule;
 import static org.daisy.pipeline.pax.exam.Options.bundlesAndDependencies;
@@ -53,7 +54,12 @@ public class LiblouisTablesTest {
 	
 	@Test
 	public void testQueryTranslator() {
-		assertTrue(liblouis.get("(locale:nl_BE)").iterator().next().asLiblouisTable()[0].toString().endsWith("manifest/nl_BE"));
+		assertTrue(serializeTable(liblouis.get("(locale:nl_BE)").iterator().next().asLiblouisTable()).endsWith("manifest/nl_BE"));
+	}
+	
+	@Test
+	public void testUnicodeBraille() {
+		assertTrue(liblouis.get("(locale:nl_BE)").iterator().next().transform("foobar").matches("[\\s\\t\\n\u00a0\u00ad\u200b\u2800-\u28ff]*"));
 	}
 	
 	private void assertNotEmpty(String message, Iterable<?> iterable) {

daisy/pipeline-scripts

pipeline-braille-utils/liblouis/liblouis-core/src/test/java/org/daisy/pipeline/braille/liblouis/LiblouisCoreTest.java
@@ -97,33 +97,32 @@ public class LiblouisCoreTest {
 	
 	@Test
 	public void testTranslate() {
-		assertEquals("foobar", provider.get("(table:'foobar.cti')").iterator().next().transform("foobar"));
+		assertEquals("⠋⠕⠕⠃⠁⠗", provider.get("(table:'foobar.cti')").iterator().next().transform("foobar"));
 	}
 	
 	@Test
 	public void testTranslateStyled() {
-		assertEquals("foobar", provider.get("(table:'foobar.cti')").iterator().next().transform("foobar", Typeform.ITALIC));
+		assertEquals("⠋⠕⠕⠃⠁⠗", provider.get("(table:'foobar.cti')").iterator().next().transform("foobar", Typeform.ITALIC));
 	}
 	
 	@Test
 	public void testTranslateSegments() {
 		LiblouisTranslator translator = provider.get("(table:'foobar.cti')").iterator().next();
-		assertEquals(new String[]{"foo","bar"}, translator.transform(new String[]{"foo","bar"}));
-		assertEquals(new String[]{"foo","","bar"}, translator.transform(new String[]{"foo","","bar"}));
+		assertEquals(new String[]{"⠋⠕⠕","⠃⠁⠗"}, translator.transform(new String[]{"foo","bar"}));
+		assertEquals(new String[]{"⠋⠕⠕","","⠃⠁⠗"}, translator.transform(new String[]{"foo","","bar"}));
 	}
 	
 	@Test
 	public void testTranslateSegmentsFuzzy() {
 		LiblouisTranslator translator = provider.get("(table:'foobar.ctb')").iterator().next();
-		assertEquals(new String[]{"fu","bar"}, translator.transform(new String[]{"foo","bar"}));
-		assertEquals(new String[]{"fu","bar"}, translator.transform(new String[]{"fo","obar"}));
-		assertEquals(new String[]{"fu","","bar"}, translator.transform(new String[]{"fo","","obar"}));
-		assertEquals(new String[]{"x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ",
-		                          "x ", "x ", "x ", "x ", "fu", "bar"},
+		assertEquals(new String[]{"⠋⠥","⠃⠁⠗"}, translator.transform(new String[]{"foo","bar"}));
+		assertEquals(new String[]{"⠋⠥","⠃⠁⠗"}, translator.transform(new String[]{"fo","obar"}));
+		assertEquals(new String[]{"⠋⠥","","⠃⠁⠗"}, translator.transform(new String[]{"fo","","obar"}));
+		assertEquals(new String[]{"⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠭ ",
+		                          "⠭ ", "⠭ ", "⠭ ", "⠭ ", "⠋⠥", "⠃⠁⠗"},
 		             translator.transform(new String[]{
 		                          "x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ", "x ",
 		                          "x ", "x ", "x ", "x ", "fo", "obar"}));
-
 	}
 	
 	@Test
@@ -139,7 +138,7 @@ public class LiblouisCoreTest {
 	@Test
 	public void testTranslateAndHyphenateSomeSegments() {
 		LiblouisTranslator translator = provider.get("(table:'foobar.cti,foobar.dic')").iterator().next();
-		assertEquals(new String[]{"foo\u00ADbar ","foobar"},
+		assertEquals(new String[]{"⠋⠕⠕\u00AD⠃⠁⠗ ","⠋⠕⠕⠃⠁⠗"},
 		             translator.transform(new String[]{"foobar ","foobar"}, new String[]{"hyphens:auto","hyphens:none"}));
 	}
 }

pipeline-braille-utils/liblouis/liblouis-tables/src/test/java/LiblouisTablesTest.java
@@ -53,7 +53,12 @@ public class LiblouisTablesTest {
 	
 	@Test
 	public void testQueryTranslator() {
-		assertTrue(provider.get("(locale:nl_BE)").iterator().next().asLiblouisTable().asURIs()[0].toString().endsWith("manifest/nl_BE"));
+		assertTrue(provider.get("(locale:nl_BE)").iterator().next().asLiblouisTable().asURIs()[1].toString().endsWith("manifest/nl_BE"));
+	}
+	
+	@Test
+	public void testUnicodeBraille() {
+		assertTrue(provider.get("(locale:nl_BE)").iterator().next().transform("foobar").matches("[\\s\\t\\n\u00a0\u00ad\u200b\u2800-\u28ff]*"));
 	}
 	
 	private void assertNotEmpty(String message, Iterable<?> iterable) {