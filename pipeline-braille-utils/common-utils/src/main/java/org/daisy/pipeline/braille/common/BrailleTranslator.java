5b5fea48336662b1d71dd9b415b22a31c6307c86
Bert Frees <bertfrees@gmail.com>
2015-08-11 16:02:04 +0200

    Make BrailleTranslator contract more strict w.r.t. white space
    see https://github.com/snaekobbi/pipeline-mod-braille/issues/52

--- pipeline-braille-utils/common-utils/src/main/java/org/daisy/pipeline/braille/common/BrailleTranslator.java
+++ pipeline-braille-utils/common-utils/src/main/java/org/daisy/pipeline/braille/common/BrailleTranslator.java
@@ -8,6 +8,20 @@ package org.daisy.pipeline.braille.common;
  * href="http://snaekobbi.github.io/braille-css-spec/#dfn-white-space-characters">white
  * space characters</a> and format characters (soft hyphens, zero width spaces
  * and no-break spaces).
+ *
+ * A BrailleTranslator must respect a number of white space processing rules:
+ * - <a
+ *   href="http://snaekobbi.github.io/braille-css-spec/master/index.html#dfn-collapsible">Collapsible</a>
+ *   white space characters may be interchanged with each other, sequences of
+ *   collapsible white space characters may be collapsed or expanded.
+ * - In some cases, collapsible white space between words may be dropped, and
+ *   soft hyphens and zero width spaces may be dropped (e.g. within
+ *   contractions).
+ * - <a
+ *   href="http://snaekobbi.github.io/braille-css-spec/master/index.html#dfn-preserved">Preserved</a>
+ *   spaces and line feeds and no-break spaces (U+00A0) must be preserved.
+ * - In no other cases may white space characters or format characters be
+ *   added or deleted.
  */
 public interface BrailleTranslator extends TextTransform {
 	
