092acb1a75a3755eb2ade50691d7b443ea4efa10
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2015-12-10 15:56:43 +0200

    strip leading and trailing whitespace

--- pipeline-braille-utils/liblouis-utils/liblouis-core/src/main/java/org/daisy/pipeline/braille/liblouis/LetterSpacingHandler.java
+++ pipeline-braille-utils/liblouis-utils/liblouis-core/src/main/java/org/daisy/pipeline/braille/liblouis/LetterSpacingHandler.java
@@ -200,8 +200,9 @@ public class LetterSpacingHandler {
 					
 				// break at SPACE, ZWSP or SHY
 				if (swoBuffer.get(i - 1) > 0) {
-					String rv = charBuffer.substring(0, i);
-						
+					// strip leading and trailing whitespace
+					String rv = charBuffer.substring(0, i).trim();
+
 					// insert hyphen glyph at SHY
 					if (swoBuffer.get(i - 1) == 0x1)
 						rv += hyphenChar;
