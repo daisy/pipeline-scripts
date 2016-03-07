daisy/pipeline-scripts

commit 2cca34dc488660639bcc4adb0d3c25c8830dd117

dotify-formatter: support footnotes

pipeline-braille-utils/css-utils/css-utils/src/main/resources/xml/make-anonymous-inline-boxes.xsl
@@ -46,14 +46,12 @@
             <xsl:sequence select="@* except @style"/>
             <xsl:sequence select="css:style-attribute(css:serialize-declaration-list($properties))"/>
             <xsl:call-template name="apply-templates">
-                <xsl:with-param name="container" select="." tunnel="yes"/>
                 <xsl:with-param name="pending-properties" select="()" tunnel="yes"/>
             </xsl:call-template>
         </xsl:copy>
     </xsl:template>
     
     <xsl:template name="apply-templates">
-        <xsl:param name="container" as="element()?" select="()" tunnel="yes"/>
         <xsl:for-each-group select="*|text()"
                             group-adjacent="boolean(descendant-or-self::css:box[@type=('block','table','table-cell')])">
             <xsl:choose>
@@ -62,17 +60,22 @@
                         <xsl:apply-templates select="."/>
                     </xsl:for-each>
                 </xsl:when>
-                <xsl:when test="$container/self::css:box[@type='inline']">
+                <xsl:when test="ancestor-or-self::css:box[@type='inline'
+                                                          and not(descendant::css:box[@type=('block','table','table-cell')])]">
                     <xsl:sequence select="current-group()"/>
                 </xsl:when>
-                <xsl:when test="matches(string-join(current-group()/string(.), ''), '^[\s&#x2800;]*$')
-                                and not(current-group()/(
-                                          descendant-or-self::css:white-space
-                                         |descendant-or-self::css:string
-                                         |descendant-or-self::css:counter
-                                         |descendant-or-self::css:text
-                                         |descendant-or-self::css:leader
-                                         |descendant-or-self::css:custom-func))">
+                <xsl:when test="not(
+                                  current-group()/
+                                    (descendant-or-self::text()[not(matches(.,'^[\s&#x2800;]*$'))]
+                                     |descendant-or-self::css:white-space
+                                     |descendant-or-self::css:string
+                                     |descendant-or-self::css:counter
+                                     |descendant-or-self::css:text
+                                     |descendant-or-self::css:content
+                                     |descendant-or-self::css:leader
+                                     |descendant-or-self::css:custom-func
+                                     )[not(ancestor::css:box[@type='inline'
+                                                             and not(descendant::css:box[@type=('block','table','table-cell')])])])">
                     <xsl:sequence select="current-group()"/>
                 </xsl:when>
                 <xsl:otherwise>

pipeline-braille-utils/css-utils/css-utils/src/test/xspec/test_make-anonymous-inline-boxes.xspec
@@ -102,4 +102,21 @@
     </x:expect>
   </x:scenario>
   
+  <x:scenario label="test_04">
+    <x:context>
+      <css:_>
+        <css:box type="inline">
+          ⠿⠿⠿
+        </css:box>
+      </css:_>
+    </x:context>
+    <x:expect label="result">
+      <css:_>
+        <css:box type="inline">
+          ⠿⠿⠿
+        </css:box>
+      </css:_>
+    </x:expect>
+  </x:scenario>
+  
 </x:description>

maven/parent/pom.xml
@@ -105,7 +105,7 @@
       <dependency>
         <groupId>org.daisy.pipeline.modules.braille</groupId>
         <artifactId>obfl-utils</artifactId>
-        <version>1.1.1</version>
+        <version>1.1.2-SNAPSHOT</version>
         <scope>test</scope>
       </dependency>
       <dependency>

pipeline-braille-utils/css-utils/css-utils/src/main/resources/xml/make-anonymous-block-boxes.xpl
@@ -17,24 +17,31 @@
             parent of a box must be another box (or a css:_ element if it's the document
             root). Inline boxes must not have non-inline descendant boxes. Table-cell boxes must
             have a parent table box and table boxes must have only table-cell child boxes. All other
-            nodes must have at least one inline box ancestor.
+            nodes must have at least one inline box ancestor. If the input represents a named flow
+            this must be indicated with a css:flow attribute on the document element.
         </p:documentation>
     </p:input>
     
     <p:output port="result">
         <p:documentation>
             Adjacent inline boxes with one or more sibling block or table boxes are grouped and
-            wrapped in an anonymous block box.
+            wrapped in an anonymous block box. Inline boxes that are top-level boxes in a named flow
+            are wrapped in an anonymous block box each and their css:anchor attributes are moved to
+            the anonymous block.
         </p:documentation>
     </p:output>
+    
+    <p:wrap match="/css:_[@flow[not(.='normal')]]/css:box[@type='inline']" wrapper="css:_box_"/>
+    <p:add-attribute match="css:_box_" attribute-name="type" attribute-value="block"/>
+    <p:label-elements match="css:_box_" attribute="css:anchor" label="*/@css:anchor"/>
+    <p:delete match="css:_box_/*/@css:anchor"/>
+    <p:rename match="css:_box_" new-name="css:box"/>
     
     <p:wrap match="css:box[@type='inline'][preceding-sibling::css:box[@type=('block','table')] or
                                            following-sibling::css:box[@type=('block','table')]]"
             group-adjacent="true()"
             wrapper="css:_box_"/>
-    
     <p:add-attribute match="css:_box_" attribute-name="type" attribute-value="block"/>
-    
     <p:rename match="css:_box_" new-name="css:box"/>
     
 </p:declare-step>

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/css-to-obfl.xpl
@@ -30,7 +30,7 @@
         <p:output port="result" sequence="true"/>
         <css:parse-stylesheet>
             <p:documentation>
-                Make css:page, css:volume, css:after, css:before, css:duplicate,
+                Make css:page, css:volume, css:after, css:before, css:footnote-call, css:duplicate,
                 css:_obfl-on-toc-start, css:_obfl-on-volume-start, css:_obfl-on-volume-end and
                 css:_obfl-on-toc-end attributes.
             </p:documentation>
@@ -106,8 +106,8 @@
     
     <pxi:recursive-parse-stylesheet-and-make-pseudo-elements>
         <p:documentation>
-            Make css:page and css:volume attributes, css:after, css:before and css:duplicate
-            pseudo-elements, and css:_obfl-on-toc-start, css:_obfl-on-volume-start,
+            Make css:page, css:volume and css:footnote-call attributes, css:after, css:before and
+            css:duplicate pseudo-elements, and css:_obfl-on-toc-start, css:_obfl-on-volume-start,
             css:_obfl-on-volume-end and css:_obfl-on-toc-end pseudo-element documents.
         </p:documentation>
     </pxi:recursive-parse-stylesheet-and-make-pseudo-elements>
@@ -170,7 +170,8 @@
         </css:parse-properties>
         <css:parse-content>
             <p:documentation>
-                Make css:string, css:text and css:counter elements from css:content attributes.
+                Make css:string, css:text and css:counter elements from css:content attributes. <!--
+                depends on make-pseudo-elements and flow-into (::footnote-call pseudo-element) -->
             </p:documentation>
         </css:parse-content>
     </p:for-each>
@@ -385,7 +386,11 @@
                 css:text-indent attributes.
             </p:documentation>
         </css:parse-properties>
-        <css:adjust-boxes/>
+        <css:adjust-boxes>
+          <p:documentation>
+            <!-- depends on make-anonymous-block-boxes -->
+          </p:documentation>
+        </css:adjust-boxes>
         <css:new-definition>
             <p:input port="definition">
                 <p:inline>

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/css-to-obfl.xsl
@@ -289,21 +289,25 @@
                     </xsl:for-each>
                 </xsl:if>
             </xsl:if>
-            <!--
-                <xsl:for-each select="collection()/*[@css:flow]">
+            <xsl:for-each select="collection()/*[@css:flow]">
                 <xsl:variable name="flow" as="xs:string" select="@css:flow"/>
-                <collection name="{$flow}">
-                <xsl:for-each select="*">
-                <item id="{@css:anchor}">
-                <xsl:apply-templates select=".">
-                <xsl:with-param name="text-transform" tunnel="yes" select="'auto'"/>
-                <xsl:with-param name="hyphens" tunnel="yes" select="'manual'"/>
-                </xsl:apply-templates>
-                </item>
-                </xsl:for-each>
-                </collection>
-                </xsl:for-each>
-            -->
+                <!--
+                    FIXME: not all flows needs to become a collection
+                -->
+                <xsl:if test="not(matches($flow,'^-obfl-on-((toc|volume)-(start|end))/'))">
+                    <collection name="{$flow}">
+                        <xsl:for-each select="*">
+                            <item id="{@css:anchor}">
+                                <xsl:apply-templates select=".">
+                                    <xsl:with-param name="text-transform" tunnel="yes" select="'auto'"/>
+                                    <xsl:with-param name="hyphens" tunnel="yes" select="'manual'"/>
+                                    <xsl:with-param name="word-spacing" tunnel="yes" select="1"/>
+                                </xsl:apply-templates>
+                            </item>
+                        </xsl:for-each>
+                    </collection>
+                </xsl:if>
+            </xsl:for-each>
             <xsl:for-each-group select="collection()/*[not(@css:flow)]" group-adjacent="string(@css:page)">
                 <xsl:variable name="layout-master" select="pxi:generate-layout-master-name(current-grouping-key())"/>
                 <xsl:for-each-group select="current-group()" group-starting-with="*[@css:page-break-before='right' or @css:counter-set-page]">
@@ -463,7 +467,7 @@
         <xsl:apply-templates select="@css:line-height|@css:text-align|@css:text-indent|@page-break-inside" mode="#current"/>
         <xsl:apply-templates select="@css:string-set|@css:_obfl-marker" mode="#current"/>
         <xsl:apply-templates mode="#current"/>
-        <!-- <xsl:apply-templates select="@css:id" mode="anchor"/> -->
+        <xsl:apply-templates select="@css:id" mode="anchor"/>
     </xsl:template>
     
     <!--
@@ -517,7 +521,7 @@
         -->
         <xsl:apply-templates select="@css:orphans|@css:widows" mode="#current"/>
         <xsl:apply-templates mode="#current"/>
-        <!-- <xsl:apply-templates select="@css:id" mode="anchor"/> -->
+        <xsl:apply-templates select="@css:id" mode="anchor"/>
     </xsl:template>
     
     <xsl:template match="css:box[@type='table']|
@@ -630,13 +634,13 @@
                     <xsl:sequence select="$attrs"/>
                     <xsl:apply-templates select="@css:string-set|@css:_obfl-marker" mode="#current"/>
                     <xsl:apply-templates mode="#current"/>
-                    <!-- <xsl:apply-templates select="@css:id" mode="anchor"/> -->
+                    <xsl:apply-templates select="@css:id" mode="anchor"/>
                 </span>
             </xsl:when>
             <xsl:otherwise>
                 <xsl:apply-templates select="@css:string-set|@css:_obfl-marker" mode="#current"/>
                 <xsl:apply-templates mode="#current"/>
-                <!-- <xsl:apply-templates select="@css:id" mode="anchor"/> -->
+                <xsl:apply-templates select="@css:id" mode="anchor"/>
             </xsl:otherwise>
         </xsl:choose>
     </xsl:template>

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/generate-obfl-layout-master.xsl
@@ -40,8 +40,22 @@
                       select="css:parse-declaration-list($default-page-stylesheet[not(@selector)]/@style)"/>
         <xsl:variable name="size" as="xs:string"
                       select="($default-page-properties[@name='size'][css:is-valid(.)]/@value, css:initial-value('size'))[1]"/>
+        <xsl:variable name="page-width" as="xs:integer" select="xs:integer(number(tokenize($size, '\s+')[1]))"/>
+        <xsl:variable name="page-height" as="xs:integer" select="xs:integer(number(tokenize($size, '\s+')[2]))"/>
+        <xsl:variable name="footnotes-properties" as="element()*"
+                      select="css:parse-declaration-list($default-page-stylesheet[@selector='@footnotes'][1]/@style)"/>
+        <xsl:variable name="footnotes-content" as="element()*"
+                      select="css:parse-content-list($footnotes-properties[@name='content'][1]/@value,())"/>
+        <xsl:variable name="footnotes-border-top" as="xs:string"
+                      select="($footnotes-properties[@name='border-top'][1]/@value,css:initial-value('border-top'))[1]"/>
+        <xsl:variable name="footnotes-max-height" as="xs:string"
+                      select="($footnotes-properties[@name='max-height'][1]/@value,css:initial-value('max-height'))[1]"/>
+        <xsl:variable name="footnotes-max-height" as="xs:integer"
+                      select="if ($footnotes-max-height='none')
+                              then $page-height idiv 2
+                              else xs:integer(number($footnotes-max-height))"/>
         <layout-master name="{$name}" duplex="{if ($duplex) then 'true' else 'false'}" page-number-variable="page"
-                       page-width="{tokenize($size, '\s+')[1]}" page-height="{tokenize($size, '\s+')[2]}">
+                       page-width="{$page-width}" page-height="{$page-height}">
             <xsl:if test="$right-page-stylesheet">
                 <!--
                     FIXME: is this influenced by initial-page-number? see https://github.com/joeha480/dotify/issues/134
@@ -66,6 +80,19 @@
                     <xsl:with-param name="stylesheet" select="$default-page-stylesheet"/>
                 </xsl:call-template>
             </default-template>
+            <xsl:if test="$footnotes-content[not(self::css:flow[@from])]">
+                <xsl:message>only flow() function supported in footnotes area</xsl:message>
+            </xsl:if>
+            <xsl:if test="count($footnotes-content[self::css:flow[@from]]) > 1">
+                <xsl:message>not more than one flow() function supported in footnotes area</xsl:message>
+            </xsl:if>
+            <xsl:for-each select="$footnotes-content[self::css:flow[@from]][1]">
+                <page-area align="bottom" max-height="{$footnotes-max-height}" collection="{@from}">
+                    <xsl:if test="$footnotes-border-top!='none'">
+                        <before><leader pattern="{$footnotes-border-top}" position="100%" align="right"/></before>
+                    </xsl:if>
+                </page-area>
+            </xsl:for-each>
         </layout-master>
     </xsl:function>
     

pipeline-braille-utils/dotify-utils/dotify-formatter/src/test/java/DotifyFormatterTest.java
@@ -131,8 +131,11 @@ public class DotifyFormatterTest {
 		                                                      new File(baseDir, "src/test/xprocspec/test_format.xprocspec"),
 		                                                      "test_obfl-to-pef",
 		                                                      new File(baseDir, "src/test/xprocspec/test_obfl-to-pef.xprocspec"),
+		                                                      // "test_dotify.formatter.impl",
+		                                                      // new File(baseDir, "src/test/xprocspec/test_dotify.formatter.impl.xprocspec"),
 		                                                      "test_propagate-page-break.xprocspec",
-		                                                      new File(baseDir, "src/test/xprocspec/test_propagate-page-break.xprocspec")),
+		                                                      new File(baseDir, "src/test/xprocspec/test_propagate-page-break.xprocspec")
+			                                      ),
 		                                      new File(baseDir, "target/xprocspec-reports"),
 		                                      new File(baseDir, "target/surefire-reports"),
 		                                      new File(baseDir, "target/xprocspec"),

commit a92fd1b7351a22ab0ff7dc7470abf23bee6aa32e

Bugfix in css:make-anonymous-inline-boxes
(broken in 2cca34d)

pipeline-braille-utils/css-utils/css-utils/src/main/resources/xml/make-anonymous-inline-boxes.xsl
@@ -52,6 +52,7 @@
     </xsl:template>
     
     <xsl:template name="apply-templates">
+        <xsl:variable name="this" as="element()" select="."/>
         <xsl:for-each-group select="*|text()"
                             group-adjacent="boolean(descendant-or-self::css:box[@type=('block','table','table-cell')])">
             <xsl:choose>
@@ -60,8 +61,8 @@
                         <xsl:apply-templates select="."/>
                     </xsl:for-each>
                 </xsl:when>
-                <xsl:when test="ancestor-or-self::css:box[@type='inline'
-                                                          and not(descendant::css:box[@type=('block','table','table-cell')])]">
+                <xsl:when test="$this/ancestor-or-self::css:box[@type='inline'
+                                                                and not(descendant::css:box[@type=('block','table','table-cell')])]">
                     <xsl:sequence select="current-group()"/>
                 </xsl:when>
                 <xsl:when test="not(

pipeline-braille-utils/css-utils/css-utils/src/test/xprocspec/test_make-anonymous-block-boxes.xprocspec
@@ -43,4 +43,40 @@
     </x:expect>
   </x:scenario>
   
+  <x:scenario label="test_02">
+    <x:call step="css:make-anonymous-block-boxes">
+      <x:input port="source">
+        <x:document type="inline">
+          <css:box type="block">
+            <css:box type="inline">
+              <css:box type="inline" name="css:before">⠿⠿</css:box>
+              ⠤⠤⠤
+            </css:box>
+            <css:box type="block" name="css:after">
+              <css:box type="inline">⠿⠿</css:box>
+            </css:box>
+          </css:box>
+        </x:document>
+      </x:input>
+    </x:call>
+    <x:context label="result">
+      <x:document type="port" port="result"/>
+    </x:context>
+    <x:expect label="result" type="compare">
+      <x:document type="inline">
+        <css:box xmlns:css="http://www.daisy.org/ns/pipeline/braille-css" type="block">
+          <css:box type="block">
+            <css:box type="inline">
+              <css:box type="inline" name="css:before">⠿⠿</css:box>
+              ⠤⠤⠤
+            </css:box>
+          </css:box>
+          <css:box type="block" name="css:after">
+            <css:box type="inline">⠿⠿</css:box>
+          </css:box>
+        </css:box>
+      </x:document>
+    </x:expect>
+  </x:scenario>
+  
 </x:description>

pipeline-braille-utils/css-utils/css-utils/src/test/xspec/test_make-anonymous-inline-boxes.xspec
@@ -119,4 +119,29 @@
     </x:expect>
   </x:scenario>
   
+  <x:scenario label="test_05">
+    <x:context>
+      <css:box type="inline">
+        <css:box type="block">
+          <css:box type="inline" name="css:before">⠿⠿</css:box>
+          ⠤⠤⠤
+          <css:box type="block" name="css:after">⠿⠿</css:box>
+        </css:box>
+      </css:box>
+    </x:context>
+    <x:expect label="result">
+      <css:_>
+        <css:box type="block">
+          <css:box type="inline">
+            <css:box type="inline" name="css:before">⠿⠿</css:box>
+          ⠤⠤⠤
+          </css:box>
+          <css:box type="block" name="css:after">
+            <css:box type="inline">⠿⠿</css:box>
+          </css:box>
+        </css:box>
+      </css:_>
+    </x:expect>
+  </x:scenario>
+  
 </x:description>