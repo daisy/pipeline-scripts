daisy/pipeline-scripts

commit f5891c6f5a428e0a55a29f3e6d4d4cdcf2aed44e

Support ::-obfl-on-toc-start, ::-obfl-on-toc-end and ::-obfl-on-volume-end

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/css-to-obfl.xsl
@@ -148,11 +148,24 @@
                                                                 <xsl:variable name="toc-name" select="generate-id($toc)"/>
                                                                 <xsl:variable name="toc-range" as="xs:string"
                                                                               select="($toc/@css:_obfl-toc-range,'document')[1]"/>
+                                                                <xsl:variable name="on-toc-start-content" as="element()*"
+                                                                              select="(collection()/*[@css:flow=concat('-obfl-on-toc-start/',
+                                                                                                                       $toc/@css:_obfl-on-toc-start)]/*,
+                                                                                       if (position()=2) then $on-first-toc-start-content else ())"/>
                                                                 <xsl:variable name="on-volume-start-content" as="element()*"
                                                                               select="if ($toc-range='document' and $toc/@css:_obfl-on-volume-start)
                                                                                       then collection()/*[@css:flow=concat('-obfl-on-volume-start/',
                                                                                                                            $toc/@css:_obfl-on-volume-start)]/*
                                                                                       else ()"/>
+                                                                <xsl:variable name="on-volume-end-content" as="element()*"
+                                                                              select="if ($toc-range='document' and $toc/@css:_obfl-on-volume-end)
+                                                                                      then collection()/*[@css:flow=concat('-obfl-on-volume-end/',
+                                                                                                                           $toc/@css:_obfl-on-volume-end)]/*
+                                                                                      else ()"/>
+                                                                <xsl:variable name="on-toc-end-content" as="element()*"
+                                                                              select="(current-group()[not(self::css:box[@type='block' and @css:_obfl-toc])],
+                                                                                       collection()/*[@css:flow=concat('-obfl-on-toc-end/',
+                                                                                                                       $toc/@css:_obfl-on-toc-end)]/*)"/>
                                                                 <toc-sequence master="{$pre-content-master}" range="{$toc-range}" toc="{$toc-name}">
                                                                     <!--
                                                                         Inserting table-of-contents here as child of toc-sequence. Will be moved to the
@@ -165,10 +178,10 @@
                                                                             <xsl:with-param name="word-spacing" tunnel="yes" select="1"/>
                                                                         </xsl:apply-templates>
                                                                     </table-of-contents>
-                                                                    <xsl:if test="exists($on-first-toc-start-content) and position()=2">
+                                                                    <xsl:if test="exists($on-toc-start-content)">
                                                                         <on-toc-start>
                                                                             <xsl:call-template name="group-inline-elements">
-                                                                                <xsl:with-param name="elements" select="$on-first-toc-start-content"/>
+                                                                                <xsl:with-param name="elements" select="$on-toc-start-content"/>
                                                                                 <xsl:with-param name="text-transform" tunnel="yes" select="'auto'"/>
                                                                                 <xsl:with-param name="hyphens" tunnel="yes" select="'manual'"/>
                                                                                 <xsl:with-param name="word-spacing" tunnel="yes" select="1"/>
@@ -185,8 +198,16 @@
                                                                             </xsl:call-template>
                                                                         </on-volume-start>
                                                                     </xsl:if>
-                                                                    <xsl:variable name="on-toc-end-content" as="element()*"
-                                                                                  select="current-group()[not(self::css:box[@type='block' and @css:_obfl-toc])]"/>
+                                                                    <xsl:if test="exists($on-volume-end-content)">
+                                                                        <on-volume-end>
+                                                                            <xsl:call-template name="group-inline-elements">
+                                                                                <xsl:with-param name="elements" select="$on-volume-end-content"/>
+                                                                                <xsl:with-param name="text-transform" tunnel="yes" select="'auto'"/>
+                                                                                <xsl:with-param name="hyphens" tunnel="yes" select="'manual'"/>
+                                                                                <xsl:with-param name="word-spacing" tunnel="yes" select="1"/>
+                                                                            </xsl:call-template>
+                                                                        </on-volume-end>
+                                                                    </xsl:if>
                                                                     <xsl:if test="exists($on-toc-end-content)">
                                                                         <on-toc-end>
                                                                             <xsl:call-template name="group-inline-elements">
@@ -553,7 +574,10 @@
     
     <xsl:template match="/*/*/@css:_obfl-toc|
                          /*/*[@css:_obfl-toc]/@css:_obfl-toc-range|
-                         /*/*[@css:_obfl-toc]/@css:_obfl-on-volume-start"
+                         /*/*[@css:_obfl-toc]/@css:_obfl-on-toc-start|
+                         /*/*[@css:_obfl-toc]/@css:_obfl-on-volume-start|
+                         /*/*[@css:_obfl-toc]/@css:_obfl-on-volume-end|
+                         /*/*[@css:_obfl-toc]/@css:_obfl-on-toc-end"
                   mode="table-of-contents"/>
     
     <xsl:template match="@css:collapsing-margins"/>
@@ -942,8 +966,11 @@
         <xsl:message>display: -obfl-toc only allowed on elements that are flowed into @begin area.</xsl:message>
     </xsl:template>
     
-    <xsl:template match="@css:_obfl-on-volume-start">
-        <xsl:message>::-obfl-on-volume-start pseudo-element only allowed on elements with display: -obfl-toc.</xsl:message>
+    <xsl:template match="@css:_obfl-on-toc-start|
+                         @css:_obfl-on-volume-start|
+                         @css:_obfl-on-volume-end|
+                         @css:_obfl-on-toc-end">
+        <xsl:message select="concat('::',replace(local-name(),'^_','-'),' pseudo-element only allowed on elements with display: -obfl-toc.')"/>
     </xsl:template>
     
     <xsl:template match="@*|*" mode="#default table-of-contents">

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/css-to-obfl.xpl
@@ -22,41 +22,51 @@
     <p:import href="http://www.daisy.org/pipeline/modules/braille/css-utils/library.xpl"/>
     <p:import href="propagate-page-break.xpl"/>
     <p:import href="shift-obfl-marker.xpl"/>
-    <p:import href="make-on-volume-start-elements.xpl"/>
+    <p:import href="make-obfl-pseudo-elements.xpl"/>
     
     <p:declare-step type="pxi:recursive-parse-stylesheet-and-make-pseudo-elements">
         <p:input port="source"/>
         <p:output port="result" sequence="true"/>
         <css:parse-stylesheet>
             <p:documentation>
-                Make css:page, css:volume, css:after, css:before, css:duplicate and
-                css:_obfl-on-volume-start attributes.
+                Make css:page, css:volume, css:after, css:before, css:duplicate,
+                css:_obfl-on-toc-start, css:_obfl-on-volume-start, css:_obfl-on-volume-end and
+                css:_obfl-on-toc-end attributes.
             </p:documentation>
         </css:parse-stylesheet>
         <p:choose>
             <p:when test="//*/@css:before|
                           //*/@css:after|
                           //*/@css:duplicate|
-                          //*/@css:_obfl-on-volume-start">
+                          //*/@css:_obfl-on-toc-start|
+                          //*/@css:_obfl-on-volume-start|
+                          //*/@css:_obfl-on-volume-end|
+                          //*/@css:_obfl-on-toc-end">
                 <css:make-pseudo-elements>
                     <p:documentation>
                         Make css:before, css:after and css:duplicate pseudo-elements from
                         css:before, css:after and css:duplicate attributes.
                     </p:documentation>
                 </css:make-pseudo-elements>
-                <p:delete match="css:duplicate/@css:_obfl-on-volume-start"/>
-                <pxi:make-on-volume-start-elements>
+                <p:delete match="css:duplicate/@css:_obfl-on-toc-start|
+                                 css:duplicate/@css:_obfl-on-volume-start|
+                                 css:duplicate/@css:_obfl-on-volume-end|
+                                 css:duplicate/@css:_obfl-on-toc-end"/>
+                <pxi:make-obfl-pseudo-elements>
                     <p:documentation>
-                        Make css:_obfl-on-volume-start pseudo-element documents from
-                        css:_obfl-on-volume-start attributes.
+                        Make css:_obfl-on-toc-start, css:_obfl-on-volume-start,
+                        css:_obfl-on-volume-end and css:_obfl-on-toc-end pseudo-element documents.
                     </p:documentation>
-                </pxi:make-on-volume-start-elements>
+                </pxi:make-obfl-pseudo-elements>
                 <p:for-each>
                     <pxi:recursive-parse-stylesheet-and-make-pseudo-elements/>
                 </p:for-each>
             </p:when>
             <p:otherwise>
+                <p:rename match="@css:_obfl-on-toc-start-ref" new-name="css:_obfl-on-toc-start"/>
                 <p:rename match="@css:_obfl-on-volume-start-ref" new-name="css:_obfl-on-volume-start"/>
+                <p:rename match="@css:_obfl-on-volume-end-ref" new-name="css:_obfl-on-volume-end"/>
+                <p:rename match="@css:_obfl-on-toc-end-ref" new-name="css:_obfl-on-toc-end"/>
             </p:otherwise>
         </p:choose>
     </p:declare-step>
@@ -96,7 +106,8 @@
     <pxi:recursive-parse-stylesheet-and-make-pseudo-elements>
         <p:documentation>
             Make css:page and css:volume attributes, css:after, css:before and css:duplicate
-            pseudo-elements, and css:_obfl-on-volume-start pseudo-element documents.
+            pseudo-elements, and css:_obfl-on-toc-start, css:_obfl-on-volume-start,
+            css:_obfl-on-volume-end and css:_obfl-on-toc-end pseudo-element documents.
         </p:documentation>
     </pxi:recursive-parse-stylesheet-and-make-pseudo-elements>
     

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/make-obfl-pseudo-elements.xpl
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <p:declare-step xmlns:p="http://www.w3.org/ns/xproc"
                 xmlns:pxi="http://www.daisy.org/ns/pipeline/xproc/internal"
-                type="pxi:make-on-volume-start-elements"
+                type="pxi:make-obfl-pseudo-elements"
                 exclude-inline-prefixes="#all"
                 version="1.0">
     
@@ -10,7 +10,7 @@
     
     <p:xslt name="xslt">
         <p:input port="stylesheet">
-            <p:document href="make-on-volume-start-elements.xsl"/>
+            <p:document href="make-obfl-pseudo-elements.xsl"/>
         </p:input>
         <p:input port="parameters">
             <p:empty/>

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/make-obfl-pseudo-elements.xsl
@@ -1,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:css="http://www.daisy.org/ns/pipeline/braille-css"
+                exclude-result-prefixes="#all"
+                version="2.0">
+    
+    <xsl:template match="@*|node()">
+        <xsl:copy>
+            <xsl:apply-templates select="@*|node()"/>
+        </xsl:copy>
+    </xsl:template>
+    
+    <xsl:template match="*[@css:_obfl-on-toc-start|
+                           @css:_obfl-on-volume-start|
+                           @css:_obfl-on-volume-end|
+                           @css:_obfl-on-toc-end]">
+        <xsl:variable name="id" select="generate-id()"/>
+        <xsl:copy>
+            <xsl:sequence select="@* except (@css:_obfl-on-toc-start|
+                                             @css:_obfl-on-volume-start|
+                                             @css:_obfl-on-volume-end|
+                                             @css:_obfl-on-toc-end)"/>
+            <xsl:if test="@css:_obfl-on-toc-start">
+                <xsl:attribute name="css:_obfl-on-toc-start-ref" select="$id"/>
+            </xsl:if>
+            <xsl:if test="@css:_obfl-on-volume-start">
+                <xsl:attribute name="css:_obfl-on-volume-start-ref" select="$id"/>
+            </xsl:if>
+            <xsl:if test="@css:_obfl-on-volume-end">
+                <xsl:attribute name="css:_obfl-on-volume-end-ref" select="$id"/>
+            </xsl:if>
+            <xsl:if test="@css:_obfl-on-toc-end">
+                <xsl:attribute name="css:_obfl-on-toc-end-ref" select="$id"/>
+            </xsl:if>
+            <xsl:apply-templates/>
+        </xsl:copy>
+        <xsl:if test="@css:_obfl-on-toc-start">
+            <xsl:result-document href="-obfl-on-toc-start/{$id}">
+                <css:_ css:flow="-obfl-on-toc-start/{$id}">
+                    <css:_obfl-on-toc-start style="{@css:_obfl-on-toc-start}"/>
+                </css:_>
+            </xsl:result-document>
+        </xsl:if>
+        <xsl:if test="@css:_obfl-on-volume-start">
+            <xsl:result-document href="-obfl-on-volume-start/{$id}">
+                <css:_ css:flow="-obfl-on-volume-start/{$id}">
+                    <css:_obfl-on-volume-start style="{@css:_obfl-on-volume-start}"/>
+                </css:_>
+            </xsl:result-document>
+        </xsl:if>
+        <xsl:if test="@css:_obfl-on-volume-end">
+            <xsl:result-document href="-obfl-on-volume-end/{$id}">
+                <css:_ css:flow="-obfl-on-volume-end/{$id}">
+                    <css:_obfl-on-volume-end style="{@css:_obfl-on-volume-end}"/>
+                </css:_>
+            </xsl:result-document>
+        </xsl:if>
+        <xsl:if test="@css:_obfl-on-toc-end">
+            <xsl:result-document href="-obfl-on-toc-end/{$id}">
+                <css:_ css:flow="-obfl-on-toc-end/{$id}">
+                    <css:_obfl-on-toc-end style="{@css:_obfl-on-toc-end}"/>
+                </css:_>
+            </xsl:result-document>
+        </xsl:if>
+    </xsl:template>
+    
+</xsl:stylesheet>

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/make-on-volume-start-elements.xsl
@@ -1,27 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-                xmlns:css="http://www.daisy.org/ns/pipeline/braille-css"
-                exclude-result-prefixes="#all"
-                version="2.0">
-    
-    <xsl:template match="@*|node()">
-        <xsl:copy>
-            <xsl:apply-templates select="@*|node()"/>
-        </xsl:copy>
-    </xsl:template>
-    
-    <xsl:template match="*[@css:_obfl-on-volume-start]">
-        <xsl:variable name="id" select="generate-id()"/>
-        <xsl:copy>
-            <xsl:sequence select="@* except @css:_obfl-on-volume-start"/>
-            <xsl:attribute name="css:_obfl-on-volume-start-ref" select="$id"/>
-            <xsl:apply-templates/>
-        </xsl:copy>
-        <xsl:result-document href="{$id}">
-            <css:_ css:flow="-obfl-on-volume-start/{$id}">
-                <css:_obfl-on-volume-start style="{@css:_obfl-on-volume-start}"/>
-            </css:_>
-        </xsl:result-document>
-    </xsl:template>
-    
-</xsl:stylesheet>

commit 23f05ba2293880da6b037db5aea28d05b80c0706

Bugfix in dotify-formatter: ::-obfl-on-toc-start sometimes in wrong place
(bug introduced in f5891c6)

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/css-to-obfl.xsl
@@ -193,9 +193,9 @@
                                                                 <xsl:variable name="toc-range" as="xs:string"
                                                                               select="($toc/@css:_obfl-toc-range,'document')[1]"/>
                                                                 <xsl:variable name="on-toc-start-content" as="element()*"
-                                                                              select="(collection()/*[@css:flow=concat('-obfl-on-toc-start/',
-                                                                                                                       $toc/@css:_obfl-on-toc-start)]/*,
-                                                                                       if (position()=2) then $on-first-toc-start-content else ())"/>
+                                                                              select="(if (position()=2) then $on-first-toc-start-content else (),
+                                                                                       collection()/*[@css:flow=concat('-obfl-on-toc-start/',
+                                                                                                                       $toc/@css:_obfl-on-toc-start)]/*)"/>
                                                                 <xsl:variable name="on-volume-start-content" as="element()*"
                                                                               select="if ($toc-range='document' and $toc/@css:_obfl-on-volume-start)
                                                                                       then collection()/*[@css:flow=concat('-obfl-on-volume-start/',