585826d2b61a991320746b36b4020ba90d9ae7bf
Bert Frees <bertfrees@gmail.com>
2016-05-04 12:45:53 +0200

    Query-based provider for BrailleUtils' FileFormat

--- pipeline-braille-utils/pef-utils/pef-core/src/test/java/PefCoreTest.java
+++ pipeline-braille-utils/pef-utils/pef-core/src/test/java/PefCoreTest.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 
+import org.daisy.braille.api.embosser.FileFormat;
 import org.daisy.braille.api.table.Table;
 import org.daisy.pipeline.braille.common.Provider;
 import static org.daisy.pipeline.braille.common.Provider.util.dispatch;
@@ -9,6 +10,7 @@ import org.daisy.pipeline.braille.common.Query;
 import org.daisy.pipeline.braille.common.Query.MutableQuery;
 import static org.daisy.pipeline.braille.common.Query.util.mutableQuery;
 import static org.daisy.pipeline.braille.common.Query.util.query;
+import org.daisy.pipeline.braille.pef.FileFormatProvider;
 import org.daisy.pipeline.braille.pef.TableProvider;
 
 import static org.daisy.pipeline.pax.exam.Options.brailleModule;
@@ -45,7 +47,7 @@ public class PefCoreTest {
 	
 	@Test
 	public void testBrailleUtilsTableCatalog() {
-		Provider<Query,Table> provider = getProvider();
+		Provider<Query,Table> provider = getTableProvider();
 		MutableQuery q = mutableQuery();
 		q.add("id", "org.daisy.braille.impl.table.DefaultTableProvider.TableType.EN_US");
 		Table table = provider.get(q).iterator().next();
@@ -53,18 +55,35 @@ public class PefCoreTest {
 	}
 	
 	@Test
-	public void testLocaleTableProvider1() {
-		Provider<Query,Table> provider = getProvider();
+	public void testLocaleBasedTableProviderEn() {
+		Provider<Query,Table> provider = getTableProvider();
 		Table table = provider.get(query("(locale:en)")).iterator().next();
 		assertEquals("FOOBAR", table.newBrailleConverter().toText("⠋⠕⠕⠃⠁⠗"));
 	}
 	
-	@Ignore // wait for BRAILLO_6DOT_031_01 support in braille-utils
 	@Test
-	public void testLocaleTableProvider2() {
-		Provider<Query,Table> provider = getProvider();
+	public void testLocaleBasedTableProviderNl() {
+		Provider<Query,Table> provider = getTableProvider();
 		Table table = provider.get(query("(locale:nl)")).iterator().next();
 		assertEquals("foobar", table.newBrailleConverter().toText("⠋⠕⠕⠃⠁⠗"));
+	}
+	
+	@Test
+	public void testBrailleUtilsFileFormatCatalog() {
+		Provider<Query,FileFormat> provider = getFileFormatProvider();
+		MutableQuery q = mutableQuery();
+		q.add("format", "org_daisy.BrailleEditorsFileFormatProvider.FileType.BRF");
+		q.add("table", "org_daisy.EmbosserTableProvider.TableType.MIT");
+		FileFormat format = provider.get(q).iterator().next();
+	}
+	
+	@Test
+	public void testBrailleUtilsEmbosserAsFileFormatCatalog() {
+		Provider<Query,FileFormat> provider = getFileFormatProvider();
+		MutableQuery q = mutableQuery();
+		q.add("embosser", "com_braillo.BrailloEmbosserProvider.EmbosserType.BRAILLO_200");
+		q.add("locale", "nl");
+		FileFormat format = provider.get(q).iterator().next();
 	}
 	
 	@Configuration
@@ -88,10 +107,20 @@ public class PefCoreTest {
 	@Inject
 	BundleContext context;
 	
-	private Provider<Query,Table> getProvider() {
+	private Provider<Query,Table> getTableProvider() {
 		List<TableProvider> providers = new ArrayList<TableProvider>();
 		try {
 			for (ServiceReference<? extends TableProvider> ref : context.getServiceReferences(TableProvider.class, null))
+				providers.add(context.getService(ref)); }
+		catch (InvalidSyntaxException e) {
+			throw new RuntimeException(e); }
+		return dispatch(providers);
+	}
+	
+	private Provider<Query,FileFormat> getFileFormatProvider() {
+		List<FileFormatProvider> providers = new ArrayList<FileFormatProvider>();
+		try {
+			for (ServiceReference<? extends FileFormatProvider> ref : context.getServiceReferences(FileFormatProvider.class, null))
 				providers.add(context.getService(ref)); }
 		catch (InvalidSyntaxException e) {
 			throw new RuntimeException(e); }
