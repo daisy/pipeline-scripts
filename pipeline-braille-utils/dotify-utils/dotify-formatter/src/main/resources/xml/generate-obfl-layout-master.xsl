daisy/pipeline-scripts

commit 495efaa329d0bd2b9415caa56380c11abd6c8867

Use braille-css in BrailleTranslator API
see https://github.com/daisy/pipeline-mod-braille/issues/100 and https://github.com/daisy/pipeline-mod-braille/issues/99

maven/bom/pom.xml
@@ -32,7 +32,7 @@
             <dependency>
                 <groupId>org.daisy.pipeline.modules.braille</groupId>
                 <artifactId>common-utils</artifactId>
-                <version>4.2.0</version>
+                <version>4.2.1-SNAPSHOT</version> <!-- should be released as 5.0.0 -->
             </dependency>
             <dependency>
                 <groupId>org.daisy.pipeline.modules.braille</groupId>
@@ -62,7 +62,7 @@
             <dependency>
                 <groupId>org.daisy.pipeline.modules.braille</groupId>
                 <artifactId>dotify-formatter</artifactId>
-                <version>1.9.0</version>
+                <version>1.9.1-SNAPSHOT</version>
             </dependency>
             <dependency>
                 <groupId>org.daisy.pipeline.modules.braille</groupId>
@@ -129,7 +129,7 @@
             <dependency>
                 <groupId>org.daisy.pipeline.modules.braille</groupId>
                 <artifactId>liblouis-core</artifactId>
-                <version>4.0.2</version>
+                <version>4.0.3-SNAPSHOT</version>
             </dependency>
             <dependency>
                 <groupId>org.daisy.pipeline.modules.braille</groupId>

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/generate-obfl-layout-master.xsl
@@ -358,29 +358,29 @@
         <xsl:choose>
             <xsl:when test="$scope=('first','page-first')">
                 <marker-reference marker="{@name}" direction="forward" scope="page"
-                                  text-style="def:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}"/>
                 <!--
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="defifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-defifndef:{$var-name}{$text-transform-decl}"/>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope=('start','page-start')">
                 <!--
                     FIXME: scope="page-content" does not work as expected
                 -->
                 <marker-reference marker="{@name}/prev" direction="forward" scope="page-content"
-                                  text-style="def:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}"/>
                 <!--
                     TODO: check that this does not match too much at the end of the page!
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="defifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-defifndef:{$var-name}{$text-transform-decl}"/>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope=('start-except-last','page-start-except-last')">
                 <!--
@@ -397,9 +397,9 @@
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="def:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}"/>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope=('last-except-start','page-last-except-start')">
                 <!--
@@ -413,7 +413,7 @@
             </xsl:when>
             <xsl:when test="$scope='spread-first'">
                 <marker-reference marker="{@name}" direction="forward" scope="spread"
-                                  text-style="def:{$var-name}{$text-transform-decl}">
+                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}">
                     <xsl:if test="$page-side='right'">
                         <xsl:attribute name="start-offset" select="'-1'"/>
                     </xsl:if>
@@ -422,20 +422,20 @@
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="defifndef:{$var-name}{$text-transform-decl}">
+                                  text-style="-dotify-defifndef:{$var-name}{$text-transform-decl}">
                     <xsl:if test="$page-side='right'">
                         <xsl:attribute name="start-offset" select="'-1'"/>
                     </xsl:if>
                 </marker-reference>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope='spread-start'">
                 <!--
                     FIXME: scope="spread-content"
                 -->
                 <marker-reference marker="{@name}/prev" direction="forward" scope="spread"
-                                  text-style="def:{$var-name}{$text-transform-decl}">
+                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}">
                     <xsl:if test="$page-side='right'">
                         <xsl:attribute name="start-offset" select="'-1'"/>
                     </xsl:if>
@@ -445,13 +445,13 @@
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="defifndef:{$var-name}{$text-transform-decl}">
+                                  text-style="-dotify-defifndef:{$var-name}{$text-transform-decl}">
                     <xsl:if test="$page-side='left'">
                         <xsl:attribute name="start-offset" select="'1'"/>
                     </xsl:if>
                 </marker-reference>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope='spread-start-except-last'">
                 <!--
@@ -471,13 +471,13 @@
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="def:{$var-name}{$text-transform-decl}">
+                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}">
                     <xsl:if test="$page-side='left'">
                         <xsl:attribute name="start-offset" select="'1'"/>
                     </xsl:if>
                 </marker-reference>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope='spread-last-except-start'">
                 <!--

pipeline-braille-utils/dotify-utils/pom.xml
@@ -21,7 +21,7 @@
     <!-- <module>dotify-core</module> -->
     <!-- <module>dotify-saxon</module> -->
     <!-- <module>dotify-calabash</module> -->
-    <!-- <module>dotify-formatter</module> -->
+    <module>dotify-formatter</module>
     <!-- <module>dotify-utils</module> -->
   </modules>
   

pom.xml
@@ -28,12 +28,12 @@
     <module>maven/bom</module>
     <!-- <module>maven/build-helper</module> -->
     <module>maven/parent</module>
-    <!-- <module>pipeline-braille-utils/common-utils</module> -->
+    <module>pipeline-braille-utils/common-utils</module>
     <module>pipeline-braille-utils/css-utils</module>
     <!-- <module>pipeline-braille-utils/pef-utils</module> -->
     <!-- <module>pipeline-braille-utils/obfl-utils</module> -->
-    <!-- <module>pipeline-braille-utils/liblouis-utils</module> -->
-    <!-- <module>pipeline-braille-utils/dotify-utils</module> -->
+    <module>pipeline-braille-utils/liblouis-utils</module>
+    <module>pipeline-braille-utils/dotify-utils</module>
     <!-- <module>pipeline-braille-utils/libhyphen-utils</module> -->
     <!-- <module>pipeline-braille-utils/texhyph-utils</module> -->
     <!-- <module>pipeline-braille-scripts</module> -->

pipeline-braille-utils/common-utils/pom.xml
@@ -22,8 +22,8 @@
       <artifactId>dotify.api</artifactId>
     </dependency>
     <dependency>
-      <groupId>org.daisy.pipeline.modules.braille</groupId>
-      <artifactId>css-core</artifactId>
+      <groupId>org.daisy.braille</groupId>
+      <artifactId>braille-css</artifactId>
     </dependency>
     <dependency>
       <groupId>com.google.guava</groupId>

pipeline-braille-utils/liblouis-utils/pom.xml
@@ -18,7 +18,7 @@
   <name>DP2 Braille Modules :: liblouis-utils</name>
 
   <modules>
-    <!-- <module>liblouis-core</module> -->
+    <module>liblouis-core</module>
     <!-- <module>liblouis-saxon</module> -->
     <!-- <module>liblouis-tables</module> -->
     <!-- <module>liblouis-calabash</module> -->

pipeline-braille-utils/common-utils/src/main/java/org/daisy/pipeline/braille/common/BrailleTranslator.java
@@ -1,5 +1,7 @@
 package org.daisy.pipeline.braille.common;
 
+import org.daisy.braille.css.SimpleInlineStyle;
+
 import org.daisy.dotify.api.translator.BrailleTranslatorResult;
 
 public interface BrailleTranslator extends Transform {
@@ -41,18 +43,31 @@ public interface BrailleTranslator extends Transform {
 	public static class CSSStyledText {
 		
 		private final String text;
-		private final String style;
+		private final SimpleInlineStyle style;
+		
+		public CSSStyledText(String text, SimpleInlineStyle style) {
+			this.text = text;
+			this.style = style;
+		}
 		
 		public CSSStyledText(String text, String style) {
 			this.text = text;
-			this.style = style;
+			if (style == null)
+				this.style = null;
+			else
+				this.style = new SimpleInlineStyle(style);
+		}
+		
+		public CSSStyledText(String text) {
+			this.text = text;
+			this.style = null;
 		}
 		
 		public String getText() {
 			return text;
 		}
 		
-		public String getStyle() {
+		public SimpleInlineStyle getStyle() {
 			return style;
 		}
 	}

pipeline-braille-utils/common-utils/src/main/java/org/daisy/pipeline/braille/common/saxon/impl/TextTransformDefinition.java
@@ -14,6 +14,7 @@ import net.sf.saxon.value.SequenceExtent;
 import net.sf.saxon.value.SequenceType;
 import net.sf.saxon.value.StringValue;
 
+import org.daisy.braille.css.SimpleInlineStyle;
 import org.daisy.pipeline.braille.common.BrailleTranslator;
 import org.daisy.pipeline.braille.common.BrailleTranslator.CSSStyledText;
 import org.daisy.pipeline.braille.common.BrailleTranslatorProvider;
@@ -106,10 +107,10 @@ public class TextTransformDefinition extends ExtensionFunctionDefinition {
 						if (style.size() != text.size())
 							throw new RuntimeException("Lengths of text and style sequences must match");
 						for (int i = 0; i < text.size(); i++)
-							styledText.add(new CSSStyledText(text.get(i), style.get(i))); }
+							styledText.add(new CSSStyledText(text.get(i), new SimpleInlineStyle(style.get(i)))); }
 					else
 						for (int i = 0; i < text.size(); i++)
-							styledText.add(new CSSStyledText(text.get(i), ""));
+							styledText.add(new CSSStyledText(text.get(i)));
 					for (BrailleTranslator t : translators.get(query))
 						try {
 							return iterableToSequence(t.fromStyledTextToBraille().transform(styledText)); }

commit 3ccc392fdc92688a5f29b2e0baef2bc8eeabfcbf

Fix bug introduced in 495efaa

maven/bom/pom.xml
@@ -62,7 +62,7 @@
             <dependency>
                 <groupId>org.daisy.pipeline.modules.braille</groupId>
                 <artifactId>dotify-formatter</artifactId>
-                <version>1.10.0</version>
+                <version>1.10.1-SNAPSHOT</version>
             </dependency>
             <dependency>
                 <groupId>org.daisy.pipeline.modules.braille</groupId>

pipeline-braille-utils/dotify-utils/dotify-formatter/src/main/resources/xml/generate-obfl-layout-master.xsl
@@ -374,29 +374,29 @@
         <xsl:choose>
             <xsl:when test="$scope=('first','page-first')">
                 <marker-reference marker="{@name}" direction="forward" scope="page"
-                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-def:{$var-name};{$text-transform-decl}"/>
                 <!--
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="-dotify-defifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-defifndef:{$var-name};{$text-transform-decl}"/>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name};{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope=('start','page-start')">
                 <!--
                     FIXME: scope="page-content" does not work as expected
                 -->
                 <marker-reference marker="{@name}/prev" direction="forward" scope="page-content"
-                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-def:{$var-name};{$text-transform-decl}"/>
                 <!--
                     TODO: check that this does not match too much at the end of the page!
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="-dotify-defifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-defifndef:{$var-name};{$text-transform-decl}"/>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name};{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope=('start-except-last','page-start-except-last')">
                 <!--
@@ -413,9 +413,9 @@
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-def:{$var-name};{$text-transform-decl}"/>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name};{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope=('last-except-start','page-last-except-start')">
                 <!--
@@ -429,7 +429,7 @@
             </xsl:when>
             <xsl:when test="$scope='spread-first'">
                 <marker-reference marker="{@name}" direction="forward" scope="spread"
-                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}">
+                                  text-style="-dotify-def:{$var-name};{$text-transform-decl}">
                     <xsl:if test="$page-side='right'">
                         <xsl:attribute name="start-offset" select="'-1'"/>
                     </xsl:if>
@@ -438,20 +438,20 @@
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="-dotify-defifndef:{$var-name}{$text-transform-decl}">
+                                  text-style="-dotify-defifndef:{$var-name};{$text-transform-decl}">
                     <xsl:if test="$page-side='right'">
                         <xsl:attribute name="start-offset" select="'-1'"/>
                     </xsl:if>
                 </marker-reference>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name};{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope='spread-start'">
                 <!--
                     FIXME: scope="spread-content"
                 -->
                 <marker-reference marker="{@name}/prev" direction="forward" scope="spread"
-                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}">
+                                  text-style="-dotify-def:{$var-name};{$text-transform-decl}">
                     <xsl:if test="$page-side='right'">
                         <xsl:attribute name="start-offset" select="'-1'"/>
                     </xsl:if>
@@ -461,13 +461,13 @@
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="-dotify-defifndef:{$var-name}{$text-transform-decl}">
+                                  text-style="-dotify-defifndef:{$var-name};{$text-transform-decl}">
                     <xsl:if test="$page-side='left'">
                         <xsl:attribute name="start-offset" select="'1'"/>
                     </xsl:if>
                 </marker-reference>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name};{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope='spread-start-except-last'">
                 <!--
@@ -487,13 +487,13 @@
                     FIXME: replace with scope="document" and remove second marker-reference
                 -->
                 <marker-reference marker="{@name}" direction="backward" scope="sequence"
-                                  text-style="-dotify-def:{$var-name}{$text-transform-decl}">
+                                  text-style="-dotify-def:{$var-name};{$text-transform-decl}">
                     <xsl:if test="$page-side='left'">
                         <xsl:attribute name="start-offset" select="'1'"/>
                     </xsl:if>
                 </marker-reference>
                 <marker-reference marker="{@name}/entry" direction="backward" scope="sequence"
-                                  text-style="-dotify-ifndef:{$var-name}{$text-transform-decl}"/>
+                                  text-style="-dotify-ifndef:{$var-name};{$text-transform-decl}"/>
             </xsl:when>
             <xsl:when test="$scope='spread-last-except-start'">
                 <!--

pipeline-braille-utils/dotify-utils/pom.xml
@@ -21,7 +21,7 @@
     <!-- <module>dotify-core</module> -->
     <!-- <module>dotify-saxon</module> -->
     <!-- <module>dotify-calabash</module> -->
-    <!-- <module>dotify-formatter</module> -->
+    <module>dotify-formatter</module>
     <!-- <module>dotify-utils</module> -->
   </modules>
   

pom.xml
@@ -33,7 +33,7 @@
     <!-- <module>pipeline-braille-utils/pef-utils</module> -->
     <!-- <module>pipeline-braille-utils/obfl-utils</module> -->
     <module>pipeline-braille-utils/liblouis-utils</module>
-    <!-- <module>pipeline-braille-utils/dotify-utils</module> -->
+    <module>pipeline-braille-utils/dotify-utils</module>
     <module>pipeline-braille-utils/libhyphen-utils</module>
     <!-- <module>pipeline-braille-utils/texhyph-utils</module> -->
     <!-- <module>pipeline-braille-scripts</module> -->