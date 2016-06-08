55ed268eada0c2b2e1cd2adea7770e43c3b47696
Bert Frees <bertfrees@gmail.com>
2016-06-08 18:18:55 +0200

    Use %indent{} pattern and maxDepth parameter to indent job messages in tests

--- pipeline-braille-scripts/dtbook-to-pef/src/test/java/XProcSpecTest.java
+++ pipeline-braille-scripts/dtbook-to-pef/src/test/java/XProcSpecTest.java
@@ -65,6 +65,8 @@ public class XProcSpecTest {
 				pipelineModule("validation-utils"),
 				// logging
 				logbackClassic(),
+				mavenBundle("org.daisy.pipeline:logging-appender:?"),
+				mavenBundle("org.daisy.pipeline:logging-activator:?"),
 				// xprocspec
 				xprocspec(),
 				mavenBundle("org.daisy.maven:xproc-engine-daisy-pipeline:?"))
